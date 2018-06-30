3306441ac8bd9a2782d3f5d811a3bef493a0335e
yahya <yahyacan55@gmail.com>
2018-07-01 01:18:50 +0300

    Initial commit

---
+++ src/main/java/com/ismek/OnlineSinavSistemiApplication.java
@@ -0,0 +1,12 @@
+package com.ismek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSinavSistemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineSinavSistemiApplication.class, args);
+	}
+}
