db8cbf47600f3668ee12fc7ac6914daf87206a5c
Lab01-29 <Lab01-29@Lab01-29>
2018-07-01 10:51:04 +0300

    Kullancıların tümünü getiren rest api yazıldı. Iterable ı listeye çeviren metod eklendi.

--- src/main/java/com/ismek/controller/RestAPIV1.java
+++ src/main/java/com/ismek/controller/RestAPIV1.java
@@ -1,18 +1,27 @@
 package com.ismek.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ismek.domain.Kullanici;
+import com.ismek.repository.KullaniciRespository;
+import com.ismek.util.Utils;
 
 @RestController
 @RequestMapping("/api/v1")
 public class RestAPIV1 {
 	
-	@RequestMapping(path="/test/{value}", method=RequestMethod.GET)
-    public String test(@PathVariable("value") String value) {
-
-        return "Sonuc : " + value;
+	@Autowired
+	private KullaniciRespository kullaniciRepository;
+	
+	@RequestMapping(path="/kullanici", method=RequestMethod.GET)
+    public List<Kullanici> findAllKullanici() {
+		Iterable<Kullanici> kullaniciIt = kullaniciRepository.findAll();
+		return Utils.toList(kullaniciIt);
     }
 
 }
